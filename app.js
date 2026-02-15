3804cc8e348cadc63d0810fcf8ac8c882392749e
kartik <kartiksharma768976@gmail.com>
2026-02-15 13:15:24 +0530

    type.added

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-var userName = "Kartik";
-var age = 21;
-var isStudent = true;
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let userName = "Kartik";
+let age = 21;
+let isStudent = true;
 console.log("Name:", userName);
 console.log("Age:", age);
 console.log("Student:", isStudent);
+//# sourceMappingURL=app.js.map
