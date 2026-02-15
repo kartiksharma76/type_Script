21c7e9c06e553698965517927099d91492d6224b
kartik <kartiksharma768976@gmail.com>
2026-02-15 13:16:22 +0530

    type.added

---
+++ src/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(person) {
+    return `helo ${person},Welcome kartik`;
+}
+const username = "kartik types scripts";
+console.log(greet(username));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+function greet(person: string): string{
+    return `helo ${person},Welcome kartik`;
+
+}
+const username: string = "kartik types scripts";
+console.log(greet(username));
